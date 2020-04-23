ddd9a3e4b0a9ccce029398f8648a04eb2d6e1333
dmortiz05 <45877723+dmortiz05@users.noreply.github.com>
2020-04-23 11:50:15 -0300

    Agregue un footer en otro archivo para hacer pruebas, junto con su CSS. Hice cambios en el index, usando cards de semantic-ui

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.2/dist/semantic.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.2/dist/semantic.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="ui menu">
+        <a class="item">Inicio</a>
+        <a class="item">Demos</a>
+        <a class="item">Nosotros</a>
+        <a class="item">Contacto</a>
+        <div class="right menu">
+            <a class="item">Iniciar sesión</a>
+            <a class="item">Registrarse</a>
+        </div>
+    </div>
+
+
+    <div class="ui items">
+        <div class="item">
+            <div class="ui small image">
+                <img src="/images/wireframe/image.png">
+            </div>
+            <div class="content">
+                <div class="header">Arrowhead Valley Camp</div>
+                <div class="meta">
+                    <span class="price">$1200</span>
+                    <span class="stay">1 Month</span>
+                </div>
+                <div class="description">
+                <p></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="ui small image">
+                <img src="/images/wireframe/image.png">
+            </div>
+            <div class="content">
+                <div class="header">Buck's Homebrew Stayaway</div>
+                <div class="meta">
+                    <span class="price">$1000</span>
+                    <span class="stay">2 Weeks</span>
+                </div>
+                <div class="description">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="ui small image">
+                <img src="/images/wireframe/image.png">
+            </div>
+            <div class="content">
+                <div class="header">Astrology Camp</div>
+                <div class="meta">
+                    <span class="price">$1600</span>
+                    <span class="stay">6 Weeks</span>
+                </div>
+                <div class="description">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+    <!--<div class="ui inverted segment">
+        <div class="ui inverted secondary pointing menu">
+            <a class="item">
+            Inicio
+            </a>
+            <a class="item">
+            Nosotros
+            </a>
+            <a class="item">
+            Contacto
+            </a>
+            <div class="right menu">
+                <a class="ui item">
+                    <div class="ui inverted button">Iniciar sesión</div>
+                </a>
+                <a class="ui item">
+                    <div class="ui inverted button">Registrarse</div>
+                </a>
+            </div>
+        </div>
+    </div>-->
+
+    <div class="footer">
+        <p>Hola, soy el footer</p>
+    </div>
+</body>
+</html>
